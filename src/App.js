bc84544445a502d7a70859a92587e795400fc196
kalashshah <202051096@iiitvadodara.ac.in>
2022-03-28 15:15:54 +0530

    :tada: initialise project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-gray-900 p-80 h-screen flex justify-center items-start flex-col">
+      <h1 className="text-5xl text-white">Hello Madg 👋</h1>
+      <p className="text-gray-400 mt-5 text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
+        eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
+      </p>
+    </div>
+  );
+};
+
+export default App;
